8e472634201325bd325bce6bb3e55a60262a54cc
Jose Estrella <estrella@cicese.mx>
2013-10-16 16:47:13 -0700

    sss 	modified:   app/assets/javascripts/application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
+
 //= require maps/locations
 //= require maps/OpenLayers
 //= require maps/jquery-2.0.3.min
