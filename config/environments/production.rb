7f01439588ec74897a93a67d52d2a89154a414fe
Jose Estrella <estrella@cicese.mx>
2013-10-16 15:05:08 -0700

    Solo en el ambiente de desarrollo 	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,31 @@ Analysistool::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( search.js )
+  #config.assets.precompile += %w( search.js )
+
+  config.assets.enabled = true
+  #le solicitamos una búsqueda todos los archivos js y css a ruby, y le pedimos que los precompile
+  config.assets.precompile << Proc.new { |path|
+    if path =~ /\.(css|js)\z/
+      full_path = Rails.application.assets.resolve(path).to_path
+      app_assets_path = Rails.root.join('app', 'assets').to_path
+      vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
+      if ((full_path.starts_with? app_assets_path) || (full_path.starts_with? vendor_assets_path)) && (!path.starts_with? '_')
+        puts "\t" + full_path.slice(Rails.root.to_path.size..-1)
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  }
+
+
+
+
+
+
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
