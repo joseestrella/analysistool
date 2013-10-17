fe86b38c6a399a28d4f4675e56e737e0e30cce4e
Jose Estrella <estrella@cicese.mx>
2013-10-16 20:13:42 -0700

    www 	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,13 +28,13 @@ config.assets.precompile << Proc.new { |path|
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #config.serve_static_assets = false //Aqui modifique
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  #config.assets.compress = true      //Aqui modifique
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false       //Aqui modifique
 
   # Generate digests for assets URLs
   config.assets.digest = true
